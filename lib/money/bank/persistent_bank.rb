133f1990adf6a61349a00d3a52341b4d1ef537df
kolen <incredible.angst@gmail.com>
2016-10-07 21:14:05 +0300

    Import rates from store lazily

--- lib/money/bank/persistent_bank.rb
+++ lib/money/bank/persistent_bank.rb
@@ -19,11 +19,17 @@ class Money
       def import_rates
         rates.clear
         cache = storage.read(@storage_key)
+        @rates_imported = true
         super(@storage_format, cache) if cache
       end
 
       def clear
         storage.delete(@storage_key)
+      end
+
+      def exchange_with(from, to_currency, &block)
+        import_rates unless @rates_imported
+        super(from, to_currency, block)
       end
     end
   end

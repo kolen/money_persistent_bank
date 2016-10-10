0ade679f917ab6d90a397c4460e867668a13b746
kolen <incredible.angst@gmail.com>
2016-10-10 17:04:31 +0300

    Add test for lazy import

--- test/persistent_bank_test.rb
+++ test/persistent_bank_test.rb
@@ -35,4 +35,15 @@ class PersistentBankTest < MiniTest::Unit::TestCase
     @reader.import_rates
     assert_nil @reader.get_rate('USD', 'AZN')
   end
+
+  def test_lazy_import
+    @writer.add_rate('USD', 'AZN', 3.00)
+    @writer.export_rates
+
+    assert_equal Money.new(30_00, 'AZN'),
+                 @reader.exchange_with(Money.new(10_00, 'USD'), 'AZN')
+    assert_raises(Money::Bank::UnknownRate) do
+      @reader.exchange_with(Money.new(10_00, 'USD'), 'AUD')
+    end
+  end
 end
